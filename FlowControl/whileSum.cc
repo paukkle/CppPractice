29607b4dd2fb43fafe3081e2ae11351238e3c56a
paukkle <paukkle@gmail.com>
2020-11-20 19:27:24 +0200

    DONE

--- FlowControl/whileSum.cc
+++ FlowControl/whileSum.cc
@@ -4,10 +4,15 @@ int main()
 {
     int sum = 0;
     int value = 0;
+
+    std::cout << "Give numbers:" << std::endl;
+
     while (value != 8)
-    {   sum += value;
-        std::cout << "Give a number:" << std::endl;
+    {   
+        sum += value;
         std::cin >> value;
     }
+    std::cout << "The sum of your numbers is "
+            << sum;
     return 0;
 }
