fb02adea937caa4306bbd8d9a1a9595eeaf04f6a
paukkle <paukkle@gmail.com>
2020-11-25 22:58:05 +0200

    added comments

--- Variables/unsigning.cc
+++ Variables/unsigning.cc
@@ -3,14 +3,14 @@
 int main()
 {
     unsigned u = 10, u2 = 42;
-    std::cout << u2 -u << std::endl;
-    std::cout << u - u2 << std::endl;
+    std::cout << u2 -u << std::endl; // returns 32
+    std::cout << u - u2 << std::endl;  // returns 42949672264
 
     int i = 10, i2 = 42;
-    std::cout << i2 - i << std::endl;
-    std::cout << i - i2 << std::endl;
+    std::cout << i2 - i << std::endl;  // returns 32
+    std::cout << i - i2 << std::endl;  // returns -32
 
-    std::cout << i - u << std::endl;
-    std::cout << u -i << std::endl;
+    std::cout << i - u << std::endl;  //returns 0
+    std::cout << u -i << std::endl;  // returns 0
     return 1;
 }
